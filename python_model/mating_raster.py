a48905f3ad4f15754c44109e56e1c8bd221387ca
zodzhao <zodzhao@berkeley.edu>
2018-04-02 14:13:19 -0700

    1, fix bug in mating_raster.py and finish implementing it. not sure if it works. 2, attempt to find where individuals are created, and changed population.py around line207 3, formatted some script files and landscape.py and population.py

--- python_model/mating_raster.py
+++ python_model/mating_raster.py
@@ -1,38 +1,47 @@
 class MatingRaster:
     def __init__(self, params):
         self.dims = params['dims']
-        mating_radius = params['mating_radius']
+        self.mating_radius = params['mating_radius']
+        self.grid_size = 2 * self.mating_radius
 
-        #
-        x = self.dims[0] / (2 * mating_radius)
-        y = self.dims[0] / (2 * mating_radius)
+        self.x = self.dims[0] / self.grid_size
+        self.y = self.dims[0] / self.grid_size
 
-        self.offset1 = [[set() for i in range(int(x))] for j in range(int(y))]
-        self.offset2 = [[set() for i in range(int(x))] for j in range(int(y))]
-
+        self.offset1 = [[set() for _ in range(int(self.x) + 1)] for j in range(int(self.y) + 1)]
+        self.offset2 = [[set() for _ in range(int(self.x) + 1)] for j in range(int(self.y) + 1)]
 
     def add(self, ind):
-        self.__add(ind.x, ind.y)
+        self.__add(ind.x, ind.y, ind)
 
     def remove(self, ind):
-        self.__remove(ind.x, ind.y)
+        self.__remove(ind.x, ind.y, ind)
         return None
 
-    def move(self, old_pos, new_pos):
-        self.__remove(old_pos[0], old_pos[1])
-        self.__add(new_pos[0], new_pos[1])
+    def move(self, old_pos, new_pos, ind):
+        self.__remove(old_pos[0], old_pos[1], ind)
+        self.__add(new_pos[0], new_pos[1], ind)
         return None
 
     #########################
     # Private Methods Below #
     #########################
 
-    def __add(self, x_pos, y_pos):
+    def __get_set(self, x_pos, y_pos):
+        # TODO: notimplemented
+        set1 = self.offset1[int(x_pos)][int(y_pos)]
+        set2 = self.offset1[int(x_pos + self.mating_radius)][int(y_pos + self.mating_radius)]
+        return set1, set2
+
+    def __add(self, x_pos, y_pos, ind):
         # private method
-        # TODO: NotImplemented
+        set1, set2 = self.__get_set(x_pos, y_pos)
+        set1.add(ind)
+        set2.add(ind)
         return None
 
-    def __remove(self, x_pos, y_pos):
+    def __remove(self, x_pos, y_pos, ind):
         # private method
-        # TODO: NotImplemented
+        set1, set2 = self.__get_set(x_pos, y_pos)
+        set1.remove(ind)
+        set2.remove(ind)
         return None
--- python_model/scripts_for_practice/PRACTICE_run_model.py
+++ python_model/scripts_for_practice/PRACTICE_run_model.py
@@ -2,17 +2,14 @@ execfile('./params.py')
 execfile('./scripts_for_practice/PRACTICE_imports_and_reloads.py')
 execfile('./scripts_for_practice/PRACTICE_create_land_genomic_arch_pop.py')
 
-
-
-
 pop.set_K(land.scapes[params['n_movement_surf_scape']])
 
 pop.genomic_arch.traits[0].s = 0.1
 
-#pop.genomic_arch.traits[0].alpha = np.array([0.25])
+# pop.genomic_arch.traits[0].alpha = np.array([0.25])
 
-for i in pop.individs.keys(): 
-    pop.individs[i].genome[pop.genomic_arch.traits[0].loci,:] = r.binomial(1,0.5,2)
+for i in pop.individs.keys():
+    pop.individs[i].genome[pop.genomic_arch.traits[0].loci, :] = r.binomial(1, 0.5, 2)
 
 
 def burn():
@@ -20,36 +17,32 @@ def burn():
     burn_in_test_t = params['burn_T_min']
     t = 0
     while break_burn_in == False:
-    	print('###############\n\n TIMESTEP %i\n' % t)
-    	pop.increment_age_stage(burn = True)
-    	#pop.mutate(params,t)
+        print('###############\n\n TIMESTEP %i\n' % t)
+        pop.increment_age_stage(burn=True)
+        # pop.mutate(params,t)
         pop.set_Nt()
-    	pop.move(land, params) 
-    	extinct = demography.pop_dynamics(land, pop, params, with_selection = False, burn = True) 
-    	t +=1
-    	if extinct == 1: 
-    		break 
-    	break_burn_in = (len(pop.Nt) > burn_in_test_t and burn_in.adf_threshold_test(pop, burn_in_test_t, 0.05) and burn_in.tt_threshold_test(pop, burn_in_test_t, 0.05))
-    
-    
-    print('~~~~~~~~~~~~~~~~\n\n\n\t\tBURN-IN COMPLETE\n\n\n~~~~~~~~~~~~~~~~~~')
-        
+        pop.move(land, params)
+        extinct = demography.pop_dynamics(land, pop, params, with_selection=False, burn=True)
+        t += 1
+        if extinct == 1:
+            break
+        break_burn_in = (len(pop.Nt) > burn_in_test_t and burn_in.adf_threshold_test(pop, burn_in_test_t,
+                                                                                     0.05) and burn_in.tt_threshold_test(
+            pop, burn_in_test_t, 0.05))
 
-def main(T, reassign_genomes = False):
+    print('~~~~~~~~~~~~~~~~\n\n\n\t\tBURN-IN COMPLETE\n\n\n~~~~~~~~~~~~~~~~~~')
+
+
+def main(T, reassign_genomes=False):
     if reassign_genomes == True:
         print('\n\nReassigning genomes...\n\n')
         genome.reassign_genomes(pop, params)
-    for t in range(T): 
+    for t in range(T):
         print('###############\n\n TIMESTEP %i\n' % t)
-        pop.increment_age_stage(burn = False)
+        pop.increment_age_stage(burn=False)
         pop.set_Nt()
         pop.move(land, params)
-        extinct = demography.pop_dynamics(land, pop, params, with_selection = True) 
-        if extinct == 1: 
+        extinct = demography.pop_dynamics(land, pop, params, with_selection=True)
+        if extinct == 1:
             break
-        pop.mutate(log = False)
-
-
-
-
-
+        pop.mutate(log=False)

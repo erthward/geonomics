d1dc21e97ce665b0f16d57f32e25026799dd3f38
Drew Hart <drew.ellison.hart@gmail.com>
2020-02-12 20:04:45 -0800

    Trying yet one more small thing to try to fix the import problem with demos in main.py.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 setuptools.setup(
     name='geonomics',
     # version num.: MAJOR.MINOR.PATCH
-    version='0.0.9',
+    version='0.0.10',
     author='Drew Ellison Hart',
     author_email='drew.ellison.hart@gmail.com',
     description='A package for landscape genomic simulation',

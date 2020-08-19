f584eba26f4cf11a5f1064290c2b3a3d02cb65fd
Drew Hart <drew.ellison.hart@gmail.com>
2020-08-19 16:41:05 -0700

    More tweaks to try to get rid of the old Yosemite tif files that appear 'stuck' in the pypi package.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 setuptools.setup(
     name='geonomics',
     # version num.: MAJOR.MINOR.PATCH
-    version='0.0.26',
+    version='0.0.27',
     author='Drew Ellison Hart',
     author_email='drew.ellison.hart@gmail.com',
     description='A package for landscape genomic simulation',
